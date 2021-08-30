ed7c91ebf3ca2f3bbc40b5f7cd7928c29dcea54e
TjarkG <tjark.gaudich@outlook.de>
2021-08-30 08:01:04 +0200

    fixed backslash warning

--- ATxmega_help.h
+++ ATxmega_help.h
@@ -43,7 +43,7 @@
     PMIC.CTRL = PMIC_LOLVLEN_bm
 
 #define ADC0_INIT                                                               \
-    ADCA.CTRLB = ADC_RESOLUTION_8BIT_gc;                                        \                                      
+    ADCA.CTRLB = ADC_RESOLUTION_8BIT_gc;                                        \
     ADCA.REFCTRL = ADC_REFSEL_INT1V_gc;                                         \
     ADCA.PRESCALER = ADC_PRESCALER_DIV32_gc;                                    \
     ADCA.CTRLA = ADC_ENABLE_bm;                                                 \
