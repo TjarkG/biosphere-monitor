7f4a750f00b05b9aa1d962e732ef65b9eb4c4b31
TjarkG <tjark.gaudich@outlook.de>
2021-09-02 08:26:18 +0200

    formated tty.h

--- tty.h
+++ tty.h
@@ -1,4 +1,15 @@
-#define SERIALTERMINAL      "/dev/ttyUSB0"
+/*
+ * tty.h
+ *
+ * Header to communicate via UART on Linux
+ *
+ * Created: 30.08.2021 16:37:17
+ *  Author: Tjark Gaudichs
+ */
+
+#ifndef tty_H_
+#define tty_H_
+
 #include <errno.h>
 #include <fcntl.h> 
 #include <stdio.h>
@@ -14,8 +25,9 @@ int set_interface_attribs(int fd, int speed)
 {
     struct termios tty;
 
-    if (tcgetattr(fd, &tty) < 0) {
-        printf("Error from tcgetattr: %s\n", strerror(errno));
+    if (tcgetattr(fd, &tty) < 0)
+    {
+        fprintf(stderr,"Error from tcgetattr: %s\n", strerror(errno));
         return -1;
     }
 
@@ -43,8 +55,9 @@ int set_interface_attribs(int fd, int speed)
     tty.c_cc[VEOL2] = 0;
     tty.c_cc[VEOF] = 0x04;
 
-    if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-        printf("Error from tcsetattr: %s\n", strerror(errno));
+    if (tcsetattr(fd, TCSANOW, &tty) != 0)
+    {
+        fprintf(stderr,"Error from tcsetattr: %s\n", strerror(errno));
         return -1;
     }
     return 0;
@@ -53,11 +66,12 @@ int set_interface_attribs(int fd, int speed)
 void startUART(char *portname)
 {
     fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) {
-        printf("Error opening %s: %s\n", portname, strerror(errno));
+    if (fd < 0)
+    {
+        fprintf(stderr,"Error opening %s: %s\n", portname, strerror(errno));
         return;
     }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
+    //baudrate 115200, 8 bits, no parity, 1 stop bit
     set_interface_attribs(fd, B115200);
 }
 
@@ -66,82 +80,24 @@ void printUART(char *in)
     char length = strlen(in);
     char wlen = write(fd, in, length);
     if (wlen != length)
-    {
-        printf("Error from write: %d, %d\n", wlen, errno);
-    }
-    tcdrain(fd);    /* delay for output */
+        fprintf(stderr,"Error from write: %d, %d\n", wlen, errno);
+    tcdrain(fd);    // delay for output
 }
 
 void getUartLine(char *buf)
 {
-    //unsigned char buf[32];
     unsigned char *p;
     int rdlen;
 
     rdlen = read(fd, buf, 32);
-    if (rdlen > 0) {
+    if (rdlen > 0)
         buf[rdlen] = 0;
-        //printf("Read %d:", rdlen);
-        // first display as hex numbers then ASCII */
-        /*for (p = buf; rdlen-- > 0; p++) {
-            printf(" 0x%x", *p);
-            //if (*p < ' ')
-                //*p = '.';   // replace any control chars
-        }*/
-        //printf("%s", buf);
-    } else if (rdlen < 0) {
+    else if (rdlen < 0)
         fprintf(stderr,"Error from read: %d: %s\n", rdlen, strerror(errno));
-    } else {  // rdlen == 0
+    else  // rdlen == 0
         fprintf(stderr,"Nothing read. EOF?\n");
-    }
     if(buf[0] == '\n')
         getUartLine(buf);
 }
 
-int test(void)
-{
-    char *portname = SERIALTERMINAL;
-    int fd;
-    int wlen;
-
-    fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) {
-        printf("Error opening %s: %s\n", portname, strerror(errno));
-        return -1;
-    }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
-    set_interface_attribs(fd, B115200);
-
-    /* simple output */
-    wlen = write(fd, "Hello!\n", 7);
-    if (wlen != 7) {
-        printf("Error from write: %d, %d\n", wlen, errno);
-    }
-    tcdrain(fd);    /* delay for output */
-
-
-    /* simple canonical input */
-    do {
-        unsigned char buf[83];
-        unsigned char *p;
-        int rdlen;
-
-        rdlen = read(fd, buf, sizeof(buf) - 1);
-        if (rdlen > 0) {
-            buf[rdlen] = 0;
-            printf("Read %d:", rdlen);
-            /* first display as hex numbers then ASCII */
-            for (p = buf; rdlen-- > 0; p++) {
-                printf(" 0x%x", *p);
-                if (*p < ' ')
-                    *p = '.';   /* replace any control chars */
-            }
-            printf("\n    \"%s\"\n\n", buf);
-        } else if (rdlen < 0) {
-            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-        } else {  /* rdlen == 0 */
-            printf("Nothing read. EOF?\n");
-        }               
-        /* repeat read */
-    } while (1);
-}
+#endif //tty_H_
