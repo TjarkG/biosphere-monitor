e7532d33b986d8686d7d2f6fbd25b6d334bee21e
TjarkG <tjark.gaudich@outlook.de>
2021-12-12 21:22:44 +0100

    added support for different package managers

--- Shell/install.sh
+++ Shell/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sudo apt-get install git gcc avr-libc binutils-avr gcc-avr avrdude make
+packagesNeeded='git gcc avr-libc binutils-avr gcc-avr avrdude make'
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 git clone https://github.com/TjarkG/biosphere-monitor
 cd biosphere-monitor
 cc PC/biosphere.c -o PC/biosphere;
